11b621c1ea60b1f6f244795f8dc188961cb4c553
최현석 <@DESKTOP-MQKO3ME>
2023-03-12 14:02:29 +0900

    1~4번 풀이

--- CodingTest/src/Buja/test_1.java
+++ CodingTest/src/Buja/test_1.java
@@ -11,6 +11,7 @@ public class test_1 {
 	//ctrl+alt+방향키(위,아래) : 코드복사(커서위치줄의 코드를 밑이나 위에 복사)
 	//alt+방향키 : 코드 순서 바꾸기
 	
+	/* 1. 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
 	public static void main(String[] args) {
 		class Solution {
 		    public int solution(int n) {
@@ -23,10 +24,60 @@ public class test_1 {
 		        return answer; // test
 		    }	
 		}
-		
-		
-		
-
 	}
+	*/
+	
+	/* 2. 정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+	 * class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        for(int i = 0; i<arr.length;i++)
+        {
+            answer += arr[i];
+        } 
+        answer /= arr.length;
+        return answer;
+    }
+}
+*/
+	/*3.  정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+	 * class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=1; i<=n;i++)
+        {
+            if(n%i==0)
+                answer +=i;
+        }
+        return answer;
+    }
+    
+}
+	 */
+	
+	/*4. 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        while(n>0)
+        {
+            answer = answer + (n%10);
+            
+                n /= 10;
+        }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
+*/
+
 
 }
+
+	
