1b8dfb033cceeb8f8a7d1fb79292440e2e984075
최현석 <@DESKTOP-MQKO3ME>
2023-03-12 14:24:55 +0900

    5번 추가

--- CodingTest/src/Buja/test_1.java
+++ CodingTest/src/Buja/test_1.java
@@ -75,6 +75,20 @@ public class Solution {
         return answer;
     }
 }
+
+/* 5. 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. */
+ * class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        
+        for(int i =0; i<n;i++)
+        {
+            answer[i]= (long)x * (i+1);
+        }
+        return answer;
+    }
+}
+ */
 */
 
 
