8ac11ab6aefe88339e10bfe1bda363caa51ba8b2
m1lotic <parkminook16@naver.com>
2023-03-15 23:21:51 +0900

    03.15 / 16번 / minook

--- CodingTest/src/Minook/test_1.java
+++ CodingTest/src/Minook/test_1.java
@@ -15,9 +15,10 @@ public class test_1 {
 	// alt+방향키 : 코드 순서 바꾸기
 
 	public static void main(String[] args) {
-
+		
 		System.out.println(solution(118372));
-
+		int[] answer = {};
+	
 	}
 
 	// 1. 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
--- CodingTest/src/Minook/test_2.java
+++ CodingTest/src/Minook/test_2.java
@@ -94,5 +94,24 @@ public class test_2 {
 //	        return answer;
 //	    }
 	
-	//6.
+	//6.array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
+	//	divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
+	
+//	public int[] solution(int[] arr, int divisor) {
+//        Arrays.sort(arr);
+//        ArrayList<Integer> answer = new ArrayList<Integer>();
+//        int cnt = 0;
+//        for(int i = 0; i<arr.length; i++){
+//            if(arr[i]%divisor==0){
+//                answer.add(arr[i]);
+//                cnt++;
+//            }
+//        }
+//        if(cnt == 0){
+//            answer.add(-1);
+//        }
+//         
+//        int[] result = answer.stream().mapToInt(Integer::intValue).toArray();
+//        return result;
+//    }
 }
