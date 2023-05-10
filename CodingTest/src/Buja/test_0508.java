9cc024b37487e5f112e7e924e8ce96c0c694fe33
최현석 <@DESKTOP-MQKO3ME>
2023-05-10 22:01:38 +0900

    5월 10일 커밋

--- CodingTest/src/Buja/test_0508.java
+++ CodingTest/src/Buja/test_0508.java
@@ -52,3 +52,71 @@ public class test_0508 {
 	
 	*/
 
+	/* 해결일 : 5월 10일
+	   문제점 : 이 문제를 보자마자
+	         아 어차피 card1 card2 길이는 얼마안되니까
+	         랜덤으로 뽑아보면 되지 않을까? 하면서
+	         고민을 했는데 ㅋㅋㅋ
+	         생각보다 시간이 오래 걸렸다.
+	         이유는 null 값 처리의 문제.
+	         금방 끝날 줄 알았는데 실패뜨길래 어떻게하지? 하다가
+	         디버깅 해보니 null값이 계속 뜨는데
+	         정해진 card1의 값을 초과했는데도 계속 right(생성한 단어) 배열에 넣으려고 해서였다.
+	         그래서 그 값이 초과되면 무조건 특정 값만 나오도록(어차피 길이는 정해져있으니)
+	         처리하고, 길이가 10일경우 경우에 따라 실패할 수 있으므로
+	         50만 회 반복을 해서(요즘 CPU 성능 좋다) 처리를 했다.
+	         재밌는 방법으로 한번 풀어봤다 아마 좋아요 박힐듯 ㅋㅋ
+	         
+
+	
+import java.util.*;
+
+class Solution {
+	public String solution(String[] cards1, String[] cards2, String[] goal) {
+		Random rd = new Random();
+		rd.setSeed(System.currentTimeMillis()); // 시드값 설정을 따로 할수도 있음
+		int a = 0;
+		int one_check = 0;
+		int two_check = 0;
+		String[] right = new String[goal.length];
+		String answer = "No";
+
+		for (int i = 0; i < 500000; i++) {
+			one_check = 0;
+			two_check = 0;
+			for (int j = 0; j < (goal.length); j++) {
+				a = rd.nextInt(2);
+
+				// 정해진 값 초과시 무조건 값 고정 처리
+				if (one_check >= cards1.length)
+					a = 1;
+				if (two_check >= cards2.length)
+					a = 0;
+
+				if (a == 0) {
+					if (one_check < cards1.length) {
+						right[j] = cards1[one_check];
+						one_check += 1;
+					}
+
+				} else if (a == 1) {
+					if (two_check < cards2.length) {
+						right[j] = cards2[two_check];
+						two_check += 1;
+					}
+				}
+
+			}
+			// System.out.println(Arrays.toString(right));
+			// 배열 일치 검사
+			if (Arrays.equals(right, goal)) {
+				answer = "Yes";
+
+				break;
+			} else {
+				answer = "No";
+			}
+		}
+		return answer;
+	}
+}*/
