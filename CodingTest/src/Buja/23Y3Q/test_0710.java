87eab0a975d7592007a3dd433f4269264f961c01
한국대표서민 <123250244+no1seomin@users.noreply.github.com>
2023-07-15 22:31:19 +0900

    7월 15일 멀리뛰기

--- CodingTest/src/Buja/23Y3Q/test_0710.java
+++ CodingTest/src/Buja/23Y3Q/test_0710.java
@@ -60,3 +60,43 @@ class Solution {
 		return answer; // 빈값(오류)
 	}
 }
+
+// 멀리 뛰기
+/*  해결일 : 7월 15일 22시 25분
+    문제점 : 이 문제는 수학 문제이다.
+            일반화를 시키고 문제를 풀면 된다고 생각해서
+            처음엔 조합으로 접근했다.
+            조합으로 풀어도 될텐데 정확한 값을 계산해주는 게 어려워서
+            정답 수열을 분석해보았다
+            1 2 3 5 8 13 21.. 
+            이것은 명백한 피보나치 수열이었다.
+            일반화 된 값은 피보나치 수열이니까
+            피보나치 수열 값을 구하는 로직을 구현했다.
+            물론 이럴 때, 1234567로 나눈 나머지니까
+            answer 값이 1234567이 넘으면, 나머지를 리턴하도록 설정해야 한다.
+            백업 코드로 내가 고민했던 코드를 첨부해두었다.
+*/
+
+class Solution {
+     
+    public long solution(int n) {
+ 
+        long answer =3;
+        long fibo_One = 1;
+        long fibo_Two = 2;
+        
+        if(n==1) return fibo_One;
+        if(n==2) return fibo_Two;
+        if(n==3) return answer;
+        
+        
+        for(int i=4;i<=n;i++)
+        {
+            fibo_One = fibo_Two%1234567;
+            fibo_Two = answer%1234567;
+           // System.out.println(fibo_One + " "+fibo_Two);
+            answer = ((fibo_One) + (fibo_Two))%1234567;
+        }
+        return answer;
+    }
+}      
