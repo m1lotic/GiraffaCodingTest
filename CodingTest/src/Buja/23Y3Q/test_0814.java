712f52a3df76649cc61d82ba42be1f537c6102fb
한국대표서민 <123250244+no1seomin@users.noreply.github.com>
2023-08-16 22:26:46 +0900

    8월 16일 소수 만들기

--- CodingTest/src/Buja/23Y3Q/test_0814.java
+++ CodingTest/src/Buja/23Y3Q/test_0814.java
@@ -18,6 +18,7 @@ public class test_0814 {
 	           중복인 부분을 걸러주고
 	           1/2가 넘지만 않게 해주면 끝
 	*/
+	/*
 	import java.util.*;
 
 	class Solution {
@@ -43,4 +44,40 @@ public class test_0814 {
 	        }
 	        return answer;
 	    }
+	}/
+	
+	// 소수 만들기
+	/* 해결일 : 8월 16일 22시 23분
+	   문제점 :  3중 for문을 지양하기 위해 시도를 해보았는데
+	            3중 for문을 사용안하고 풀기는 매우 어렵다..
+	            안그러면 조합써야하는데..
+	            조합쓰는게 이 풀이보다 더 복잡하다
+	*/
+	/*
+	class Solution {
+	    public int solution(int[] nums) {
+	        int answer = 0;
+	        int check =0; //소수체크(약수가 몇개인지?)
+	        int sum = 0;
+	        
+	        // 3중 for문([1,2,3],4) 선택을 위함
+	        for(int i=0;i<nums.length;i++)  //  (1),2,3,4
+	        {
+	            for(int j=i+1;j<nums.length;j++) // 1,(2),3,4
+	            {
+	                for(int z=j+1;z<nums.length;z++)// 1,2,(3),4
+	                {
+	                    sum += (nums[i]+nums[j]+nums[z]); // 수의 3개 합 구하기
+	                    for(int k=2;k<=sum;k++) // 수가 소수인 지 검사
+	                        if(sum%k==0)   check +=1;
+	                    if(check==1)   answer +=1; // 소수인가 판결
+	                    System.out.println(sum+" "+check);
+	                    check=0;
+	                    sum=0; // 값을 초기화함
+	                }
+	            }
+	        }
+	        return answer;
+	    }
 	}
+	*/
