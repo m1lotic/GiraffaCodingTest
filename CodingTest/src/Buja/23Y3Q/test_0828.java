04ec391519eeefa7f1c0b090dc74d6501bbb1612
한국대표서민 <123250244+no1seomin@users.noreply.github.com>
2023-08-30 21:48:09 +0900

    8월 30일 약수의 개수와 덧셈

--- CodingTest/src/Buja/23Y3Q/test_0828.java
+++ CodingTest/src/Buja/23Y3Q/test_0828.java
@@ -55,3 +55,29 @@ public class test_0828 {
 	    }
 	}
 	 */
+	
+	// 약수의 개수와 덧셈
+	/* 해결일 : 8월 30일 21시 46분
+	   문제점 : 정답이 맞는데 풀이가 틀려서 다시 봤다.
+	            만약 약수를 /2를 해서 구한다면
+	            자기자신의 수가 빠지므로
+	            약수의 개수는 0개에서 카운트가 아니고 1에서
+	            카운트를 시켜줘야한다.
+	            
+	
+	class Solution {
+	    public int solution(int left, int right) {
+	        int answer = 0;
+	        int divid_cnt=1; // 자기 자신 수
+	        while(left<=right){
+	            for(int i = 1; i<=left/2;i++)    if(left%i==0) divid_cnt++;
+	            
+	            System.out.println(divid_cnt);
+	            if(divid_cnt%2==0) answer += left;
+	            else answer -= left;
+	            divid_cnt=1;
+	            left++;
+	        }
+	        return answer;
+	    }
+	}*/
